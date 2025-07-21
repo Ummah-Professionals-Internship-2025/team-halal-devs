b86b718f43ee9e57f4ad80abe3f3f4aee2a70f7d
Souzen Khan <souzenk@uci.edu>
2025-07-21 11:13:05 -0700

    added remove button, fixed css

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./App.css";
+import "./CreateMeetingForm.css";
 import "bootstrap/dist/css/bootstrap.css";
+import "./CreateMeetingForm.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
