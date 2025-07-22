8923c5fa42735905c587dc31c713e26fb23672a1
Souzen Khan <souzenk@uci.edu>
2025-07-22 09:27:48 -0700

    fixing merge conflicts

--- backend/meetings/views.py
+++ backend/meetings/views.py
@@ -6,10 +6,6 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from .models import Meeting
 from .serializers import MeetingSerializer
-<<<<<<< HEAD
-
-=======
->>>>>>> 788f58f (Creating a form in front-end that takes input for meeting requests and connects to backend api)
 def home(request):
     return HttpResponse("Welcome to the API. Visit /admin/ or /api/")
 
@@ -18,7 +14,6 @@ def api_root(request):
 
 @method_decorator(csrf_exempt, name='dispatch')
 class CreateMeeting(APIView):
-<<<<<<< HEAD
     authentication_classes = [] 
     permission_classes = []
 
@@ -36,12 +31,3 @@ class CreateMeeting(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-=======
-    def post(self, request):
-        serializer = MeetingSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
->>>>>>> 788f58f (Creating a form in front-end that takes input for meeting requests and connects to backend api)
