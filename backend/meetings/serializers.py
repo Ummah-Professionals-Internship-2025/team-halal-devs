5644f3d23fbc8e9103f32ac832b07349cc661d52
Ameerah <ameerahgadatia17@gmail.com>
2025-08-04 13:36:09 -0400

    display count of available users for each time slot

--- backend/meetings/serializers.py
+++ backend/meetings/serializers.py
@@ -5,65 +5,60 @@ from .models import Meeting, TimeOption, AvailabilityResponse, AvailabilityEntry
 class TimeOptionSerializer(serializers.ModelSerializer):
   
   class Meta:
-      model = TimeOption
-      fields = ['id', 'start_time', 'end_time']
+     model = TimeOption
+     fields = ['id', 'start_time', 'end_time']
 
 
 
 class MeetingSerializer(serializers.ModelSerializer):
-  # For creation: time_options are write_only (sent by client on POST)
-  time_options = TimeOptionSerializer(many=True, write_only=True)
+ # For creation: time_options are write_only (sent by client on POST)
+ time_options = TimeOptionSerializer(many=True, write_only=True)
+
+ class Meta:
+     model = Meeting
+     fields = ['id', 'name', 'description', 'created_at', 'time_options']
 
 
-  class Meta:
-      model = Meeting
-      fields = ['id', 'name', 'description', 'created_at', 'time_options']
-
-
-  def create(self, validated_data):
-      time_options_data = validated_data.pop('time_options')
-      meeting = Meeting.objects.create(**validated_data)
-      for option in time_options_data:
-          TimeOption.objects.create(
-              meeting=meeting,
-              start_time=option['start_time'],
-              end_time=option['end_time']
-          )
-      return meeting
+ def create(self, validated_data):
+     time_options_data = validated_data.pop('time_options')
+     meeting = Meeting.objects.create(**validated_data)
+     for option in time_options_data:
+         TimeOption.objects.create(
+             meeting=meeting,
+             start_time=option['start_time'],
+             end_time=option['end_time']
+         )
+     return meeting
 
 
 
 class MeetingDetailSerializer(serializers.ModelSerializer):
-  # For reading: time_options included (no write_only)
-  time_options = TimeOptionSerializer(many=True)
+ # For reading: time_options included (no write_only)
+ time_options = TimeOptionSerializer(many=True)
 
-
-  class Meta:
-      model = Meeting
-      fields = ['name', 'description', 'time_options']
+ class Meta:
+     model = Meeting
+     fields = ['name', 'description', 'time_options']
 
 
 
 class AvailabilityEntrySerializer(serializers.ModelSerializer):
-  # This will serialize the time_option field for the AvailabilityEntry model
-  time_option = TimeOptionSerializer()  # Serialize TimeOption data
+ # This will serialize the time_option field for the AvailabilityEntry model
+ time_option = TimeOptionSerializer()  # Serialize TimeOption data
 
-
-  class Meta:
-      model = AvailabilityEntry
-      fields = ['time_option']
+ class Meta:
+     model = AvailabilityEntry
+     fields = ['time_option']
 
 
 
 class AvailabilityResponseSerializer(serializers.ModelSerializer):
-  # This will serialize all the entries (time options) for the given availability response
-  entries = AvailabilityEntrySerializer(many=True)  # A participant can have multiple time options selected
- 
-  # Ensure participant_name and email are included
-  participant_name = serializers.CharField()
-  email = serializers.EmailField()
+ # This will serialize all the entries (time options) for the given availability response
+ entries = AvailabilityEntrySerializer(many=True)  # A participant can have multiple time options selected
+ # Ensure participant_name and email are included
+ participant_name = serializers.CharField()
+ email = serializers.EmailField()
 
-
-  class Meta:
-      model = AvailabilityResponse
-      fields = ['participant_name', 'email', 'entries']
+ class Meta:
+     model = AvailabilityResponse
+     fields = ['participant_name', 'email', 'entries']
--- backend/meetings/urls.py
+++ backend/meetings/urls.py
@@ -3,11 +3,10 @@ from . import views
 
 
 urlpatterns = [
-   path('', views.api_root, name='api-root'),
-   path('meetings/', views.CreateMeeting.as_view(), name='create-meeting'),
-   path('meetings/<uuid:meeting_id>/', views.MeetingDetailView.as_view(), name='meeting-detail'),
-  
-   # Separate GET and POST routes for availability responses
-   path('meetings/<uuid:meeting_id>/availability-responses/', views.AvailabilityResponseListView.as_view(), name='availability-response-list'),  # GET
-   path('meetings/<uuid:meeting_id>/availability-responses/create/', views.AvailabilityResponseCreate.as_view(), name='availability-response-create'),  # POST
+  path('', views.api_root, name='api-root'),
+  path('meetings/', views.CreateMeeting.as_view(), name='create-meeting'),
+  path('meetings/<uuid:meeting_id>/', views.MeetingDetailView.as_view(), name='meeting-detail'),
+  path('meetings/<uuid:meeting_id>/availability-responses/', views.AvailabilityResponseListView.as_view(), name='availability-response-list'),  # GET
+  path('meetings/<uuid:meeting_id>/availability-responses/create/', views.AvailabilityResponseCreate.as_view(), name='availability-response-create'),  # POST
+  path('meetings/<uuid:meeting_id>/availability-summary/', views.AvailabilitySummaryView.as_view(), name='availability-summary')
 ]
