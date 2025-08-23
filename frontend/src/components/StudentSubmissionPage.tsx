93b65f59bc734b63f12102180d6b3c65e866a425
kyuterose <arwa.arsalan@gmail.com>
2025-08-23 15:18:46 -0500

    added css styling to calendar

---
+++ frontend/src/components/StudentSubmissionPage.tsx
@@ -0,0 +1,63 @@
+// DELETE THIS COMPONENT
+// import "./studentsubmissionpage.css";
+// // import { useState } from "react";
+
+// const StudentSubmissionPage = () => {
+//   const data = [
+//     {
+//       name: "Dur e Khawlah",
+//       industry: "IT",
+//       date: "July 13, 2025",
+//       status: "PENDING",
+//     },
+//     {
+//       name: "Souzen Khan",
+//       industry: "Business",
+//       date: "July 13, 2025",
+//       status: "PENDING",
+//     },
+//   ];
+//   //   const [name, setName] = useState("");
+//   //   const [email, setEmail] = useState("");
+//   //   const [phone, setPhone] = useState("");
+//   //   const [industry, setIndustry] = useState("");
+//   //   const [seeking, setSeeking] = useState("");
+//   //   const [academicYear, setAcademicYear] = useState("");
+//   //   const [resume, setResume] = useState<File | null>(null);
+
+//   //   const [currentStep, setCurrentStep] = useState("Info");
+//   //   const steps = ["Info", "Availability", "Wrap-up", "Submit"];
+
+//   return (
+//     <div className="info-form">
+//       <h3 className="meeting-page-title">Student Submission</h3>
+
+//       {/* Two Column Layout */}
+//       <div className="form-container">
+//         {/* Left Column */}
+//         <div className="form-container-child">
+//           <label className="label">
+//             Name <span className="required">*</span>
+//           </label>
+//         </div>
+
+//         {/* Right Column */}
+//         <div className="form-container-child">
+//           <label className="label">
+//             Industry <span className="required">*</span>
+//           </label>
+
+//           <label className="label">
+//             Sign-up Date <span className="required">*</span>
+//           </label>
+
+//           <label className="label">
+//             Status <span className="required">*</span>
+//           </label>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default StudentSubmissionPage;
