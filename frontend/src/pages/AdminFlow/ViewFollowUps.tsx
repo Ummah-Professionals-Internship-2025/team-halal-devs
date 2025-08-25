e6a220f8bd1cb04ad69dd40ef752af18b50ca86a
Ameerah <ameerahgadatia17@gmail.com>
2025-08-24 23:53:28 -0400

    updated sidebar, imported icons, updated status button designs

--- frontend/src/pages/AdminFlow/ViewFollowUps.tsx
+++ frontend/src/pages/AdminFlow/ViewFollowUps.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import AdminLayout from "../../components/AdminLayout";
 
 const ViewFollowUps: React.FC = () => {
   return (
-    <div className="professional-page-container">
-      <h1 className="student-page-title">Follow-ups</h1>
-    </div>
+    <AdminLayout>
+      <h1 className="student-page-title">Follow-Ups</h1>
+    </AdminLayout>
   );
 };
 
